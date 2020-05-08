ecf3c0fbb365e71bf7479a91ae1a6b0fea77abf0
Võ Thị Tú <52452160+vothitu@users.noreply.github.com>
2020-05-08 13:58:55 +0700

    update

---
+++ src/Project/Col.java
@@ -0,0 +1,15 @@
+package Project;
+public class Col {
+	public int n;
+	public Col(int n)
+	{
+		this.n=n;
+	}
+	public int Tall()
+	{
+		return n*8+10;
+	}
+	public int value() {
+		return n;
+	}
+}
